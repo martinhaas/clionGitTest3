419e0231afab585b76c20123f2bb5e7e31547eee
aa97092 <martin_haas@sadiatech.com>
2019-08-28 16:12:30 -0400

    Initial Commit

---
+++ CMakeFiles/clionGitTest3.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/aa97092/CLionProjects/clionGitTest3/main.cpp" "C:/Users/aa97092/CLionProjects/clionGitTest3/CMakeFiles/clionGitTest3.dir/main.cpp.obj"
+  "C:/Users/aa97092/CLionProjects/clionGitTest3/src/testClass.cpp" "C:/Users/aa97092/CLionProjects/clionGitTest3/CMakeFiles/clionGitTest3.dir/src/testClass.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "c:/Root/msys64/mingw64/include"
+  "c:/Root/msys64/mingw64/lib"
+  "include"
+  "include/boostDES"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/clionGitTest3.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/clionGitTest3.dir/main.cpp.obj"
+  "CMakeFiles/clionGitTest3.dir/src/testClass.cpp.obj"
+  "clionGitTest3.pdb"
+  "clionGitTest3.exe"
+  "clionGitTest3.exe.manifest"
+  "libclionGitTest3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/clionGitTest3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
